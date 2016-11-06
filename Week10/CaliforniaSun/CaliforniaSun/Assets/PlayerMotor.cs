ad0b0728dfdcf888e7ab702293c291c800515fe8
luif <luif@usc.edu>
2016-11-05 20:07:11 -0700

    Added multiplayer scene

---
+++ Week10/CaliforniaSun/CaliforniaSun/Assets/PlayerMotor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+
+
+	Vector3 movement;
+
+	void Update () {
+		movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		transform.Translate(movement * Time.deltaTime * 8f);
+	}
+}
---
+++ Week10/CaliforniaSun/CaliforniaSun/Assets/SetupLocalPlayer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+	[SyncVar]
+	public string playerName = "unnamed";
+
+	TextMesh nameMesh;
+
+	void OnGUI()
+	{
+
+		if (isLocalPlayer)
+		{
+			playerName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), playerName);
+
+			if(GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "change")) {
+				CmdChangeName(playerName);
+			}
+
+		}
+	}
+
+	void Start()
+	{
+		if (isLocalPlayer)
+			GetComponent<PlayerMotor>().enabled = true;
+
+		nameMesh = GetComponentInChildren<TextMesh>();
+	}
+
+	[Command]
+	public void CmdChangeName(string newName) {
+		playerName = newName;
+	}
+
+
+	void Update()
+	{
+		if (isLocalPlayer)
+			nameMesh.text = playerName;
+	}
+
+
+}
